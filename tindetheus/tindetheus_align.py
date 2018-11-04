e191769bf876f720d30e7076c4bea73bb049a23e
Charles Jekel <cjekel@gmail.com>
2018-11-03 22:30:59 -0400

    make tindetheus.align pep8

--- tindetheus/tindetheus_align.py
+++ tindetheus/tindetheus_align.py
@@ -9,8 +9,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# The facenet implmentation has been hard coded into tindetheus. This has been
+# The facenet implementation has been hard coded into tindetheus. This has been
 # hardcoded into tindetheus for the following reasons: 1) there is no setup.py
 # for facenet yet. 2) to prevent changes to facenet from breaking tindetheus.
 #
@@ -39,8 +39,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -59,7 +59,6 @@ import imageio
 from skimage.transform import resize
 import sys
 import os
-import argparse
 import tensorflow as tf
 import numpy as np
 import tindetheus.facenet_clone.facenet as facenet
@@ -67,34 +66,36 @@ from tindetheus.facenet_clone.align import detect_face
 import random
 from time import sleep
 
+
 def main(input_dir='database', output_dir='database_aligned', image_size=182,
-                        margin=44, gpu_memory_fraction=1, random_order=False):
+         margin=44, gpu_memory_fraction=1, random_order=False):
     sleep(random.random())
     output_dir = os.path.expanduser(output_dir)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     # Store some git revision info in a text file in the log directory
-    src_path,_ = os.path.split(os.path.realpath(__file__))
+    # src_path, _ = os.path.split(os.path.realpath(__file__))
     cwd = os.getcwd()
     facenet.store_revision_info(cwd, output_dir, ' '.join(sys.argv))
     dataset = facenet.get_dataset(input_dir)
 
-
     print('Creating networks and loading parameters')
 
     with tf.Graph().as_default():
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
-        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)  # noqa: E501
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options,
+                          log_device_placement=False))
         with sess.as_default():
             pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
 
-    minsize = 20 # minimum size of face
-    threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
-    factor = 0.709 # scale factor
+    minsize = 20  # minimum size of face
+    threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+    factor = 0.709  # scale factor
 
-    # Add a random key to the filename to allow alignment using multiple processes
+    # Add a random key to the filename to allow alignment using multiple
+    # processes
     random_key = np.random.randint(0, high=99999)
-    bounding_boxes_filename = os.path.join(output_dir, 'bounding_boxes_%05d.txt' % random_key)
+    bounding_boxes_filename = os.path.join(output_dir, 'bounding_boxes_%05d.txt' % random_key)  # noqa: E501
 
     with open(bounding_boxes_filename, "w") as text_file:
         nrof_images_total = 0
@@ -110,7 +111,8 @@ def main(input_dir='database', output_dir='database_aligned', image_size=182,
             for image_path in cls.image_paths:
                 nrof_images_total += 1
                 filename = os.path.splitext(os.path.split(image_path)[1])[0]
-                output_filename = os.path.join(output_class_dir, filename+'.png')
+                output_filename = os.path.join(output_class_dir,
+                                               filename+'.png')
                 print(image_path)
                 if not os.path.exists(output_filename):
                     try:
@@ -119,44 +121,45 @@ def main(input_dir='database', output_dir='database_aligned', image_size=182,
                         errorMessage = '{}: {}'.format(image_path, e)
                         print(errorMessage)
                     else:
-                        if img.ndim<2:
+                        if img.ndim < 2:
                             print('Unable to align "%s"' % image_path)
                             text_file.write('%s\n' % (output_filename))
                             continue
                         if img.ndim == 2:
                             img = facenet.to_rgb(img)
-                        img = img[:,:,0:3]
+                        img = img[:, :, 0:3]
 
-                        bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+                        bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)  # noqa: E501
 
                         nrof_faces = bounding_boxes.shape[0]
-                        if nrof_faces>0:
-                            det = bounding_boxes[:,0:4]
+                        if nrof_faces > 0:
+                            det = bounding_boxes[:, 0:4]
                             img_size = np.asarray(img.shape)[0:2]
-                            if nrof_faces>1:
-                                bounding_box_size = (det[:,2]-det[:,0])*(det[:,3]-det[:,1])
+                            if nrof_faces > 1:
+                                bounding_box_size = (det[:, 2]-det[:, 0])*(det[:, 3]-det[:, 1])  # noqa: E501
                                 img_center = img_size / 2
-                                offsets = np.vstack([ (det[:,0]+det[:,2])/2-img_center[1], (det[:,1]+det[:,3])/2-img_center[0] ])
-                                offset_dist_squared = np.sum(np.power(offsets,2.0),0)
-                                index = np.argmax(bounding_box_size-offset_dist_squared*2.0) # some extra weight on the centering
-                                det = det[index,:]
+                                offsets = np.vstack([(det[:, 0]+det[:, 2])/2-img_center[1], (det[:, 1]+det[:, 3])/2-img_center[0]])  # noqa: E501
+                                offset_dist_squared = np.sum(np.power(offsets, 2.0), 0)  # noqa: E501
+                                index = np.argmax(bounding_box_size - offset_dist_squared*2.0)  # noqa: E501  # some extra weight on the centering
+                                det = det[index, :]
                             det = np.squeeze(det)
                             bb = np.zeros(4, dtype=np.int32)
                             bb[0] = np.maximum(det[0]-margin/2, 0)
                             bb[1] = np.maximum(det[1]-margin/2, 0)
                             bb[2] = np.minimum(det[2]+margin/2, img_size[1])
                             bb[3] = np.minimum(det[3]+margin/2, img_size[0])
-                            cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
-                            scaled = resize(cropped, (image_size, image_size), mode='constant')
+                            cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
+                            scaled = resize(cropped, (image_size, image_size),
+                                            mode='constant')
                             nrof_successfully_aligned += 1
                             imageio.imwrite(output_filename, scaled)
-                            text_file.write('%s %d %d %d %d\n' % (output_filename, bb[0], bb[1], bb[2], bb[3]))
+                            text_file.write('%s %d %d %d %d\n' % (output_filename, bb[0], bb[1], bb[2], bb[3]))  # noqa: E501
                         else:
                             print('Unable to align "%s"' % image_path)
                             text_file.write('%s\n' % (output_filename))
 
     print('Total number of images: %d' % nrof_images_total)
-    print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
+    print('Number of successfully aligned images: %d' % nrof_successfully_aligned)  # noqa: E501
 
 
 if __name__ == '__main__':
