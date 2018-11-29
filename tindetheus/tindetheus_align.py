67cbdc9888102567597ef4d0c0ad03550ceb8172
cjekel <cjekel@gmail.com>
2018-11-29 15:52:00 -0500

    convert image to uint8 before saving

--- tindetheus/tindetheus_align.py
+++ tindetheus/tindetheus_align.py
@@ -57,6 +57,7 @@ from __future__ import print_function
 
 import imageio
 from skimage.transform import resize
+from skimage import img_as_ubyte
 import sys
 import os
 import tensorflow as tf
@@ -152,7 +153,9 @@ def main(input_dir='database', output_dir='database_aligned', image_size=182,
                             scaled = resize(cropped, (image_size, image_size),
                                             mode='constant')
                             nrof_successfully_aligned += 1
-                            imageio.imwrite(output_filename, scaled)
+                            # convert image to uint8 before saving
+                            imageio.imwrite(output_filename,
+                                            img_as_ubyte(scaled))
                             text_file.write('%s %d %d %d %d\n' % (output_filename, bb[0], bb[1], bb[2], bb[3]))  # noqa: E501
                         else:
                             print('Unable to align "%s"' % image_path)
