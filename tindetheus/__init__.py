f40c12c8712c657e77199ded5973755f66a0a6fa
Charles Jekel <cjekel@gmail.com>
2019-07-28 16:57:15 -0400

    change import of submodules

--- tindetheus/__init__.py
+++ tindetheus/__init__.py
@@ -1,7 +1,7 @@
 from .tindetheus import *  # noqa F401
-from . import machine_learning  # noqa F401
-from . import image_processing  # noqa F401
-from . import tinder_client  # noqa F401
+import .machine_learning as machine_learning # noqa F401
+import .image_processing as image_processing # noqa F401
+import .tinder_client as tinder_client # noqa F401
 import os
 
 # add rudimentary version tracking
