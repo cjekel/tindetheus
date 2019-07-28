489ad5e294e35cdf2fb0f855fdbc6168d5cf132d
Charles Jekel <cjekel@gmail.com>
2019-07-28 17:18:02 -0400

    add simple tests for imports

--- tindetheus/__init__.py
+++ tindetheus/__init__.py
@@ -1,4 +1,6 @@
 from . import tindetheus  # noqa F401
+from . import tindetheus_align  # noqa F401
+from . import export_embeddings  # noqa F401
 from . import machine_learning  # noqa F401
 from . import image_processing  # noqa F401
 from . import tinder_client  # noqa F401
