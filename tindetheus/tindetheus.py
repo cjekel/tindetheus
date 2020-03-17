0f38cc4989d0ea3d34d8a705e07daca759ad2ba6
Vikash Kothary <kothary.vikash@gmail.com>
2020-03-17 23:30:09 +0000

    Replace config.txt parser with config.py values

--- tindetheus/tindetheus.py
+++ tindetheus/tindetheus.py
@@ -41,6 +41,7 @@ except AttributeError:
     import urllib2
     urllib_HTTP_Error = urllib2.HTTPError
 
+from tindetheus import config
 from tindetheus import export_embeddings
 from tindetheus import tindetheus_align
 from tindetheus.tinder_client import client
@@ -252,41 +253,13 @@ Optional arguments will overide config.txt settings.
 
 def command_line_run():
     # settings to look for
-    defaults = {'facebook_token': None,
-                'XAuthToken': None,
-                'model_dir': '20170512-110547',
-                'image_batch': 1000,
-                'distance': 5,
-                'likes': 100,
-                'retries': 20}
-    # check for a config file first
-    integer_list = ['image_batch', 'distance', 'likes', 'retries']
-    string_list = ['model_dir', 'XAuthToken', 'facebook_token']
-    try:
-        with open('config.txt') as f:
-            lines = f.readlines()
-            for line in lines:
-                my_line_list = line.split(' ')
-                if len(my_line_list) == 1:
-                    print('config.txt did not parse this line: \n', line)
-                elif len(my_line_list) == 2:
-                    print('config.txt did not parse this line: \n', line)
-                    # Note that if you get this print, you need a space around
-                    # both sides of the equal sign in config.txt
-                elif len(my_line_list) > 2:
-                    if my_line_list[0] in integer_list:
-                        defaults[my_line_list[0]] = int(my_line_list[2].strip('\n'))  # noqa E501
-                    elif my_line_list[0] in string_list:
-                        defaults[my_line_list[0]] = my_line_list[2].strip('\n').strip('\r')  # noqa E501
-
-    except FileNotFoundError:
-        print('No config.txt found')
-        print('You must create a config.txt file as specified in the README')
-        # create_new_config = input('Would you like us to create a
-        # new config.txt file? (y,n) : ')
-        # if create_new_config == 'y' or create_new_config == 'Y':
-        #     print('Creating a new config...')
-
+    defaults = {'facebook_token': config.FACEBOOK_AUTH_TOKEN,
+                'XAuthToken': config.TINDER_AUTH_TOKEN,
+                'model_dir': config.TINDETHEUS_MODEL_DIR,
+                'image_batch': config.TINDETHEUS_IMAGE_BATCH,
+                'distance': config.TINDETHEUS_DISTANCE,
+                'likes': config.TINDETHEUS_LIKES,
+                'retries': config.TINDETHEUS_RETRIES}
     # parse the supplied arguments
     args = parse_arguments(sys.argv[1:], defaults)
 
