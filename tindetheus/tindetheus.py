aacf78e320c3bd836cf42abd75ec88351ed6bd69
cjekel <cjekel@gmail.com>
2020-03-28 16:19:51 -0700

    switch to __VERSION__ within tindetheus import

--- tindetheus/tindetheus.py
+++ tindetheus/tindetheus.py
@@ -41,6 +41,7 @@ except AttributeError:
     import urllib2
     urllib_HTTP_Error = urllib2.HTTPError
 
+from tindetheus import __version__
 from tindetheus import config
 from tindetheus import export_embeddings
 from tindetheus import tindetheus_align
@@ -53,10 +54,6 @@ try:
     FileNotFoundError
 except NameError:
     FileNotFoundError = IOError
-
-# add version tracking
-VERSION_FILE = os.path.join(os.path.dirname(__file__), 'VERSION')
-__version__ = open(VERSION_FILE).read().strip()
 
 
 def catch_http_error(e, args, retries, facebook_token, x_auth_token):
